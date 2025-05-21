5029f713eb369096b12618680099fad28a71e41d
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-05-21 16:58:29 +0300

    Did ex 1.7

--- part01/unicafe/src/App.jsx
+++ part01/unicafe/src/App.jsx
@@ -16,23 +16,77 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [all, setAll] = useState([0])
+  const [entries, setEntries] = useState([])
+
   const handleReset = () => {
   setGood(0)
   setNeutral(0)
   setBad(0)
+
+  setAll(0)
+  setEntries([])
   }
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+
+    setEntries(entries.concat(1))
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+    
+    setEntries(entries.concat(0))
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+    
+    setEntries(entries.concat(-1))
+  }
+
+  const calculateAverage = () => {
+    if (entries.length === 0) {
+      return 0;
+    }
+
+    const sum = entries.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum / entries.length
+  }
+
+  const calculatePositives = () => {
+    if (entries.length === 0) {
+      return 0;
+    }
+
+    const posCount= entries.filter(num => num > 0).length;
+    return (posCount / entries.length) * 100
+  }
+
+  //console.log(calculateAverage())
+  //console.log(calculatePositives())
 
   return (
     <div>
       <Header text="give feeback"/>
-      <Button onClick={() => setGood(good + 1)} text='good'/>
-      <Button onClick={() => setNeutral(neutral + 1)} text='neutral'/>
-      <Button onClick={() => setBad(bad + 1)} text='bad'/>
+      <Button onClick={handleGoodClick} text='good'/>
+      <Button onClick={handleNeutralClick} text='neutral'/>
+      <Button onClick={handleBadClick} text='bad'/>
             
       <Header text="statistics"/>
       <div> good {good}</div>
       <div> neutral {neutral}</div>
-      <div> bad {bad}</div>      
+      <div> bad {bad}</div>
+
+      <div> all {entries.length}</div>
+      <div> average {calculateAverage()}</div>
+      <div> positive {calculatePositives()} % </div>
+      
+      <Button onClick={handleReset} text='Reset'/>     
     </div>
   )
 }
