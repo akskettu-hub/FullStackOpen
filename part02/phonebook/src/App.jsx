6a231541f5454c655a2f50ac7647f828627bf5e2
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-05-27 15:47:15 +0300

    Did ex 2.6. Created dir phonebook.

---
+++ part02/phonebook/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Entry = ({person}) => <p key={person.name}>{person.name} </p>
+
+const Entries = ({persons}) => {
+  return (
+    <div>
+      {persons.map(person => (<Entry person={person}/>))}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const addNewName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const nameObject = {
+      name: newName
+      
+      
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNoteChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Entries persons={persons} />
+    </div>
+  )
+}
+
+export default App
