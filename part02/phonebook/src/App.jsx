45b902323280b22489f21af98c83e0311617e9df
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-05-28 14:53:12 +0300

    Did ex 2.11. Added db.json and installed axios etc. as per course material

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ value, onChange }) => <div>filter shown with: <input value={value} onChange={onChange}/></div>
-
-
 
 const PersonForm = ({ onSubmit, nameProps, numberProps }) => {
   return (
@@ -13,7 +12,6 @@ const PersonForm = ({ onSubmit, nameProps, numberProps }) => {
           <button type="submit">add</button>
         </div>
       </form>
-
   )
 }
 
@@ -36,15 +34,22 @@ const Entries = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setnewNumber] = useState('')
   const [newFilter, setnewFilter] = useState('')
+
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'notes')
+  
   
   const addNewName = (event) => {
     event.preventDefault()
@@ -95,10 +100,6 @@ const App = () => {
           value: newNumber,
           onChange: handleNumberChange
         }}
-        valueName={newName} 
-        onChangeName={handleNameChange} 
-        valueNumber={newNumber} 
-        onChangeNumber={handleNumberChange}
       />
       
       <h3>Numbers</h3>
