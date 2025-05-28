f30e6dd79418ea9349d859ff9b80247fa5136578
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-05-28 12:52:03 +0300

    Did ex 2.9. Phonebook now has filtering. Note: If filtering string is empty, all entries are shown

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -2,22 +2,32 @@ import { useState } from 'react'
 
 const Entry = ({person}) => <p>{person.name} {person.number}</p>
 
-const Entries = ({persons}) => {
+// Returns all entries that pass the filter. If filter is an empty string, all entries are returned. 
+const Entries = ({ persons, filter }) => {
   return (
     <div>
-      {persons.map(person => (<Entry key={person.name} person={person}/>))}
+      {persons
+        .filter(person =>
+          !filter || person.name.toLowerCase().includes(filter.toLowerCase()) 
+        )
+        .map(person => (
+          <Entry key={person.name} person={person}/>
+        ))
+      }
     </div>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setnewNumber] = useState('')
+  const [newFilter, setnewFilter] = useState('')
   
   const addNewName = (event) => {
     event.preventDefault()
@@ -47,9 +57,16 @@ const App = () => {
     setnewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setnewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input value={newFilter} onChange={handleFilterChange}/></div>
+      <h2>Add a new number</h2>
       <form onSubmit={addNewName}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -58,7 +75,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Entries persons={persons} />
+      <Entries persons={persons} filter={newFilter}/>
     </div>
   )
 }
