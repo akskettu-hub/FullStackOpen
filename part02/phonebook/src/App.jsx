0670102c0171b15eb7f9f19868d072d402aa36a3
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-05-28 11:35:51 +0300

    Did ex 2.8. Added Number field and its hander to phonebook

--- part02/phonebook/src/App.jsx
+++ part02/phonebook/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Entry = ({person}) => <p>{person.name} </p>
+const Entry = ({person}) => <p>{person.name} {person.number}</p>
 
 const Entries = ({persons}) => {
   return (
@@ -12,24 +12,28 @@ const Entries = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', 
+      number: '040-1234567'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setnewNumber] = useState('')
   
   const addNewName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if (persons.some(person => person.name === nameObject.name)) {
-      console.log(`${nameObject.name} already in phonebook`);
+      console.log(`${nameObject.name} already in phonebook`)
       alert(`${nameObject.name} is already added to phonebook.`)
     } else {
-      console.log(`${nameObject.name} not in phonebook`);
-      setPersons(persons.concat(nameObject));
-      setNewName('');
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setnewNumber('')
     }
   }
 
@@ -38,13 +42,17 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setnewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
