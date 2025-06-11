ee31ba7da3f22bfcd2367b31864f65e952ddd717
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-06-11 16:30:23 +0300

    Did ex 2.18. Did some refactoring of components.

--- part02/countries/src/App.jsx
+++ part02/countries/src/App.jsx
@@ -5,38 +5,75 @@ const SearchField = ({ value, onChange }) => <div>Find countries: <input value={
 
 const Country = ({ country }) => <div> {country} </div>
 
-const Countries = ({ filteredCountries }) => {
-  if (!filteredCountries) {
-    return null
-  }
-
-  return (
-      filteredCountries.map(country => (
-        <Country key={country} country={country} />
-      ))
+const Countries = ({ countries }) => {
+  console.log(countries)
+  if (countries.length > 1 && countries.length < 11) {
+    console.log('matching 1-10');
+    
+    return (
+      <div>
+        {
+          countries.map(country => (
+          <Country key={country} country={country} />
+          ))
+        }
+      </div>
     )
+  } 
+  return null
 }
 
-  
+const CountryCardFlag = ({ flagUrl }) => {
+  console.log(flagUrl)
+  //fetch picture
+  //return renderable image
+  return <img src={flagUrl} alt="Country flag not found" />
+}
 
-const CountryCard = ({ country }) => {
-  if (country === null) {
+const CountryCardLanguages = ({ languages }) => {
+  console.log(Object.values(languages));
+  
+  return (
+    <div>
+      <h2>Languages</h2>
+      <ul>
+        {
+          Object.values(languages)
+            .map((lang, index) => (
+              <li key={index}>{lang}</li>
+            ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+const CountryCard = ({ countryData }) => {
+  if (!countryData) {
     return null
   }
+
+  console.log(countryData);
+  console.log(countryData.capital); 
 
   return (
     <div>
-      <h1>{country}</h1>
+      <h1>{countryData.name.common}</h1>
+      <div>Capital {countryData.capital.join(', ')}</div>
+      <div>Area {countryData.area}</div>
+      
+      <CountryCardLanguages languages={countryData.languages}/>
+      <CountryCardFlag flagUrl={countryData.flags.png}/>  
     </div>
   )
-
 }
 
 const App = () => {
   const [newQuery, setNewQuery] = useState('')
   const [countries, setCountries]= useState([])
   const [filteredCountries, setFilteredCountries]= useState([])
-  const [matchingcountry, setMatchingCountry]= useState(null)
+  const [matchingCountry, setMatchingCountry]= useState(null)
+  const [matchingCountryData, setMatchingCountryData]= useState(null)
 
   useEffect(() => {
     console.log("effect")
@@ -49,42 +86,51 @@ const App = () => {
       })
   }, [])
   
-  //console.log(countries);
-
   const handleQueryChange = (event) => {
-    setQueryChange(event)
-    filterResults()
-    
-  }
-
-  const setQueryChange = (event) => {
     console.log(event.target.value)
     setNewQuery(event.target.value)
-    
   }
 
-  const filterResults = () => {
+  useEffect(() => {
     const matching = countries.filter(country => !newQuery || country.toLowerCase().includes(newQuery.toLowerCase()))
-    console.log('state:', newQuery)
-    console.log('matching:', matching)
 
-  }
+    setFilteredCountries(matching)
 
-  const handleMatchingCountry = (country) => {
-    countriesService
-      .getCountry(country)
-      .then(response => {
-        console.log('found', country);
-        console.log(response.data);
-        setMatchingCountry(response.data)
-      })
-    setMatchingCountry()
-  }
+    if (matching.length === 1) {
+      countriesService
+        .getCountry(matching[0])
+        .then(response => {
+          console.log('promise fulfilled');
+          setMatchingCountry(matching[0])
+        })
+    } else setMatchingCountry(null)
+  }, [newQuery])
+
+  useEffect(() =>{
+
+    if (matchingCountry) {
+      // fetch country data
+      // set country data
+      console.log('matching country found')
+      countriesService
+        .getCountry(matchingCountry)
+        .then(response => {
+          console.log('promise fulfilled: matching coutry');
+          setMatchingCountryData(response.data)
+        })
+    } else {
+      // set country data to null
+      console.log('matching country set to empty array')
+      setMatchingCountryData(null)
+    }
+  }, [matchingCountry])
 
   return (
     <div>
       <SearchField value={newQuery} onChange={handleQueryChange}/>
-      <Countries countries={filteredCountries}/>
+      <div>{newQuery}</div>
+      <Countries countries={filteredCountries} />
+      <CountryCard countryData={matchingCountryData}/>
     </div>
   )
 }
