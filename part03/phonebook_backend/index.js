64c6adb2b723722e9946ab8c9150bd616046a1a7
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-06-12 16:11:46 +0300

    did ex 3.5. Added functionality to add people to phonebook. Generates random id, which is not a good way of doing that.

--- part03/phonebook_backend/index.js
+++ part03/phonebook_backend/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -55,6 +55,40 @@ app.get('/api/persons/:id', (request, response) => {
   } else {    
     response.status(404).end()  
   }
+})
+
+const generateId = () => {
+    const maxId = persons.length * 100
+    const newId = Math.floor(Math.random() * maxId) + 1
+
+  return String(newId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || null,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
