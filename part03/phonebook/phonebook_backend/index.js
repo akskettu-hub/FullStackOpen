60037a63656c525213c105d4a9220b5e49b3a05f
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-06-17 15:09:53 +0300

    Did ex 3.15.

--- part03/phonebook/phonebook_backend/index.js
+++ part03/phonebook/phonebook_backend/index.js
@@ -69,22 +69,17 @@ app.get('/info', (request, response) => {
   )
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(note => note.id === id)
-  if (person) {
-    response.json(person)
-  } else {    
-    response.status(404).end()  
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-    const maxId = persons.length * 100
-    const newId = Math.floor(Math.random() * maxId) + 1
-
-  return String(newId)
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -111,12 +106,45 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
-  response.status(204).end()
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
