b3b38e48c65075ce6b12073a37196730fdedbcd3
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-06-19 17:48:15 +0300

    Did ex 4.5. Added a function that returns an object with the name of the author with the most number of blogs and the number of blogs. Added a function that tests that this works.

--- part04/blog/utils/list_helper.js
+++ part04/blog/utils/list_helper.js
@@ -8,10 +8,30 @@ const totalLikes = (blogs) => {
 
 const favouriteBlog = (blogs) => {
     return blogs.reduce((maxLikes, blog) => maxLikes.likes > blog.likes ? maxLikes : blog, -1)
-
 }
+
+const mostBlogs = (blogs) => {
+    let authors = []
+    
+    for (let i = 0; i < blogs.length; i++) {
+        
+        authorInAuthors = authors.find(item => item.author === blogs[i].author)
+        if (!authorInAuthors) {
+            // Author not in authors. push an element to authors. blogs initialised as 1
+            let authorObj = { author: blogs[i].author, blogs: 1}
+            authors.push(authorObj)
+        } else {
+            // Increment the blogs field of the apporiate author
+            authorInAuthors.blogs +=1
+        }
+    }
+    //console.log(authors)
+    return authors.reduce((maxBlogs, author) => maxBlogs.blogs < author.blogs ? author : maxBlogs )
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs
 }
