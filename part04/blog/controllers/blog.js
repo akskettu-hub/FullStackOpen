b4d1968a51646ff0c79857c421928b0962a6283a
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-06-20 15:42:01 +0300

    Changed before each to handle any length array in async.

--- part04/blog/controllers/blog.js
+++ part04/blog/controllers/blog.js
@@ -2,13 +2,18 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const {info, error} = require('../utils/logger')
 
-blogsRouter.get('/', (request, response) => {
+/*blogsRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs)
   })
+})*/
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
     const blog = new Blog({
@@ -18,11 +23,12 @@ blogsRouter.post('/', (request, response, next) => {
     likes: body.likes,
   })
 
-  blog.save()
-    .then((result) => {
-    response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- part04/blog/tests/blog_api.test.js
+++ part04/blog/tests/blog_api.test.js
@@ -12,28 +12,36 @@ const initalBlogs = [
         title: "Gleaming the Cube",
         author: "Michael Trapezoid",
         url: "blog.com/cube_trapezoid",
-        likes: "8",
+        likes: 8,
     },
     {
         title: "Running Backend",
         author: "John James",
         url: "blog.com/running_backend_james",
-        likes: "1",
+        likes: 1,
     },
     {
         title: "Beans",
         author: "Gerals Garbanzo",
         url: "blog.com/beans",
-        likes: "12",
+        likes: 12,
+    },
+    {
+        title: "More on Beans, by popular request",
+        author: "Gerals Garbanzo",
+        url: "blog.com/beans2",
+        likes: 12,
     }
 ]
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let noteObject = new Blog(initalBlogs[0])
-    await noteObject.save()
-    noteObject = new Blog(initalBlogs[1])
-    await noteObject.save()
+
+    const blogObjects = initalBlogs
+        .map(blog => new Blog(blog))
+
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
 })
 
 describe('api tests', () => {
@@ -46,8 +54,8 @@ describe('api tests', () => {
 
     test('all blog are returned', async () => {
         const response = await api.get('/api/blogs')
-
-        assert.strictEqual(response.body.length, 2)
+        
+        assert.strictEqual(response.body.length, initalBlogs.length)
     })
 
     after(async () => {
