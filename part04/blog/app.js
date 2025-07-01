9b47613e77bdf9d76297b69a45cd0a7c0692dd0e
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-01 14:19:23 +0300

    Did ex 4.18. Implemented token auth.

--- part04/blog/app.js
+++ part04/blog/app.js
@@ -5,6 +5,7 @@ const {info, error} = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blog')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part04/blog/controllers/blog.js
+++ part04/blog/controllers/blog.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -17,11 +18,26 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')  
+  if (authorization && authorization.startsWith('Bearer ')) {    
+    return authorization.replace('Bearer ', '')  
+  }  
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)  
+    if (!decodedToken.id) {    
+      return response.status(401).json({ error: 'token invalid' })  
+    }  
+    const user = await User.findById(decodedToken.id)
+
     //const user = await User.findById(body.userId)
-    const users = await User.find({})
-    const user = users[0]
+    //const users = await User.find({})
+    //const user = users[0]
 
     if (!user) {
       return response.status(400).json({ error: 'userId missing or not  valid' })
