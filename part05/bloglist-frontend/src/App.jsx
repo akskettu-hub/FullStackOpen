d7847c0bd7ef2d2bcc6d459ad388b6a45327df87
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-02 13:33:48 +0300

    Did ex 5.2. Implemented local storage for user info. Implemented logout button and handler which delets locally stored user login data.

--- part05/bloglist-frontend/src/App.jsx
+++ part05/bloglist-frontend/src/App.jsx
@@ -18,6 +18,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {    
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')    
+    if (loggedUserJSON) {      
+      const user = JSON.parse(loggedUserJSON)      
+      setUser(user)      
+      blogService.setToken(user.token)    
+    } 
+   }, [])
+
   const handleLogin = async (event) => {    
     event.preventDefault()    
     //console.log('logging in with', username, password)
@@ -26,6 +35,11 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -66,6 +80,11 @@ const App = () => {
     </form>      
   )
 
+  const logoutHandler = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogAppUser')
+  }
+
   if (user === null) {
     return (
       <div>
@@ -82,7 +101,10 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{`${user.name} logged in`}</p>
+      <p>
+        {`${user.name} logged in`}
+        <button onClick={logoutHandler}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
