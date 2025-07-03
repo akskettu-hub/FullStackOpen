305df3e85fff63c36df3511274d59ffa2c53282b
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-03 08:24:52 +0300

    Copied backend from part04 to part05

---
+++ part05/bloglist-backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const {info, error} = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+info('Connecting to MongoDB', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        error('Error connecting to MongoDB', error)
+    })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+//app.use(middleware.userExtractor)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
