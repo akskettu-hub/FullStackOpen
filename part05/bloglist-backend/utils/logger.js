2ef1071add31135288afef818aeab48ca6deef94
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-07 09:13:15 +0300

    Did ex 5.18. Wrote tests for loging in. Created helper function for logging in.

--- part05/bloglist-backend/utils/logger.js
+++ part05/bloglist-backend/utils/logger.js
@@ -1,12 +1,16 @@
 const info = (...params) => {
     if (process.env.NODE_ENV !== 'test') {
         console.log(...params)
+    } else {
+        console.log(...params)
     }
     
 }
 
 const error = (...params) => {
     if (process.env.NODE_ENV !== 'test') {
+        console.log(...params)
+    } else {
         console.log(...params)
     }
 }
--- part05/e2e/tests/blog_app.spec.js
+++ part05/e2e/tests/blog_app.spec.js
@@ -1,8 +1,19 @@
 const { describe, test, expect, beforeEach } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'testuser',
+        password: 'password123'
+      }
+    })
+
     await page.goto('http://localhost:5173')
+
   })
 
   test('front page can be opened', async ({ page }) => {
@@ -22,6 +33,23 @@ describe('Blog app', () => {
     
     await expect(page.getByTestId('username')).toBeVisible()
     await expect(page.getByTestId('password')).toBeVisible()
-    //await expect(locator).toBeVisible()
+  })
+
+  describe('Loging in ', () => {
+    test('successfull with correct credentials', async ({ page }) => {
+      await loginWith(page, 'testuser', 'password123')
+
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('fails with incorrect credentials', async ({ page }) => {
+      await loginWith(page, 'testuser', 'worngpassword')
+
+      await expect(page.getByText('Wrong credentials')).toBeVisible()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toContainText('Wrong credentials')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
   })
 })
