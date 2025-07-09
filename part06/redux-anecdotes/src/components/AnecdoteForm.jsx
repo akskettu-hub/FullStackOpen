d9008eab6d3ab722c8731ae1a5c7a55e08ac048a
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-09 09:51:51 +0300

    did ex 6.15. Added anecdotes now stored on the backend

--- part06/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part06/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,19 @@
 import {  useDispatch } from 'react-redux'
-import {  newAnecdote } from '../reducers/anecdoteReducer'
+import {  createAnecdote } from '../reducers/anecdoteReducer'
 import { notify } from '../reducers/notificationReducer'
+import noteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
         console.log('New anecdote:', content)
-        dispatch(newAnecdote(content))
+
+        const newAnecdote = await noteService.createNew(content)
+        dispatch(createAnecdote(newAnecdote))
         dispatch(notify('ADD', content))
         
     }
