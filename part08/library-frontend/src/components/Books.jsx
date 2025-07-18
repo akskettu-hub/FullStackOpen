710a123d895acec1c894eba631883bb6fa70ff6c
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-18 19:04:31 +0300

    Did ex 8.19 books can now be filtered by genre.

--- part08/library-frontend/src/components/Books.jsx
+++ part08/library-frontend/src/components/Books.jsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@apollo/client";
 import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = (props) => {
+  const [filter, setFilter] = useState("");
   const result = useQuery(ALL_BOOKS, {
     skip: !props.show,
   });
@@ -16,9 +18,16 @@ const Books = (props) => {
 
   const books = result.data.allBooks;
 
+  const genres = [...new Set(books.map((b) => b.genres).flat())];
+
   return (
     <div>
       <h2>books</h2>
+      {filter && (
+        <div>
+          Books in genre <b>{filter}</b>
+        </div>
+      )}
 
       <table>
         <tbody>
@@ -27,15 +36,23 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((book) => filter === "" || book.genres.includes(filter))
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      {genres.map((lable, index) => (
+        <button key={index} onClick={() => setFilter(lable)}>
+          {lable}
+        </button>
+      ))}
+      <button onClick={() => setFilter("")}>all genres</button>
     </div>
   );
 };
