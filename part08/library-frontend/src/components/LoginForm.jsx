a4b6e9e249eab48dd4641e0ed82ab256a58dcb2a
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-18 18:18:03 +0300

    Did ex. 8.18. Implemented loging in and out. token stored locally. Adding books and editing birthyear work.

---
+++ part08/library-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,61 @@
+import { useMutation } from "@apollo/client";
+import { useEffect, useState } from "react";
+import { LOGIN } from "../queries";
+
+const LoginForm = (props) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message);
+    },
+  });
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value;
+      props.setToken(token);
+      localStorage.setItem("library-user-token", token);
+    }
+  }, [result.data]);
+
+  if (!props.show) {
+    return null;
+  }
+
+  const submit = (event) => {
+    event.preventDefault();
+    console.log("hit submit", username, password);
+
+    login({ variables: { username, password } });
+
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <div>
+          username
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
--- part08/library-frontend/src/queries.js
+++ part08/library-frontend/src/queries.js
@@ -36,7 +36,9 @@ export const CREATE_BOOK = gql`
       genres: $genres
     ) {
       title
-      author
+      author {
+        name
+      }
     }
   }
 `;
@@ -46,6 +48,14 @@ export const UPDATE_BIRTHYEAR = gql`
     editAuthor(name: $name, setBornTo: $born) {
       name
       born
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `;
