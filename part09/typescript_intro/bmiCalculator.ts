549ee6ea0782f2d233aae46ba30074b7475a1dde
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2025-07-21 16:37:02 +0300

    Did ex 9.5. Added endpoint for bmi calculator and modified bmi calculator to work both as a module and from command line or when run as main.

--- part09/typescript_intro/bmiCalculator.ts
+++ part09/typescript_intro/bmiCalculator.ts
@@ -3,6 +3,16 @@ interface bmiValues {
   weight: number;
 }
 
+interface bmiQueryResponseValues {
+  height: number;
+  weight: number;
+  bmi: string;
+}
+
+interface bmiQueryResponseError {
+  error: string;
+}
+
 const parseArguments = (args: string[]): bmiValues => {
   if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
@@ -32,13 +42,57 @@ const calculateBmi = (height: number, weight: number): string => {
   return "caluation failed";
 };
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "BMI calulator failed. ";
-  if (error instanceof Error) {
-    errorMessage += "Error: " + error.message;
+const parseQueryArgs = (queryArgs: any): bmiValues => {
+  if (
+    typeof queryArgs === "object" &&
+    "height" in queryArgs &&
+    "weight" in queryArgs
+  ) {
+    if (!isNaN(Number(queryArgs.height)) && !isNaN(Number(queryArgs.weight))) {
+      return {
+        height: Number(queryArgs.height),
+        weight: Number(queryArgs.weight),
+      };
+    } else throw new Error("arguments are not numbers");
+  } else
+    throw new Error("query must be object that has fields height and weight");
+};
+
+const bmiCalculator = (
+  queryArgs: any
+): bmiQueryResponseValues | bmiQueryResponseError => {
+  try {
+    const { height, weight } = parseQueryArgs(queryArgs);
+    const bmi = calculateBmi(height, weight);
+    console.log(bmi);
+    return {
+      height,
+      weight,
+      bmi,
+    };
+  } catch (error: unknown) {
+    let errorMessage = "BMI calulator failed. ";
+    if (error instanceof Error) {
+      errorMessage += "Error: " + error.message;
+    }
+    console.log(errorMessage);
+    return {
+      error: "malformatted parameters",
+    };
   }
-  console.log(errorMessage);
+};
+
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "BMI calulator failed. ";
+    if (error instanceof Error) {
+      errorMessage += "Error: " + error.message;
+    }
+    console.log(errorMessage);
+  }
 }
+
+export default bmiCalculator;
--- part09/typescript_intro/index.ts
+++ part09/typescript_intro/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import bmiCalculator from "./bmiCalculator";
+
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
+});
+
+app.get("/bmi", (req, res) => {
+  res.send(bmiCalculator(req.query));
 });
 
 const PORT = 3003;
